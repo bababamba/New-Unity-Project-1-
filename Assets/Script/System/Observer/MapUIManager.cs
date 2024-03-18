fb33fa01af314e8f7bca073ae82a8fc07f10ec10
SeriousTroLL <silverrock726@gmail.com>
2024-03-19 08:29:06 +0900

    이벤트 및 상점 1차 구현
    - 이벤트 구현 : csv파일을 읽고, 이벤트명 / 설명 / 선택지를 UI에 표시. 선택한 선택지 / 이벤트 종류에 따라 대응하는 결과 텍스트로 변경.
    - 상점 구현 : 11종의 무기 중 6종을 무작위 선별하여 아이템 이미지 표시. 추후 플레이어와 연동해 가격 / 잔고 비교, 구매 이후 업그레이드 적용 등 할 예정.

--- Assets/Script/System/Observer/MapUIManager.cs
+++ Assets/Script/System/Observer/MapUIManager.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using Random = System.Random;
 
 public class MapUIManager : MonoBehaviour
 {
+    public TextAsset EventData;
+
     public GameObject[] PoPUpScreen;
     public GameObject[] Screen;
     public static MapUIManager manager;
+    public Random random = new Random();
 
     private bool isAnyUIOpen = false;
-
 
     void Start()
     {
@@ -35,21 +38,62 @@ public class MapUIManager : MonoBehaviour
         }
     }
 
-
     public void OpenUI(int num)
     {
         if(isAnyUIOpen)
             return;
 
         PoPUpScreen[num].SetActive(true);
+        if (num == 0)
+            PoPUpScreen[num].GetComponent<EventUI>().Init(random.Next(0, 5));
+        else if (num == 1)
+            PoPUpScreen[num].GetComponent<ShopUI>().OpenShop();
         PoPUpScreen[num].GetComponent<RectTransform>().anchoredPosition = Vector3.zero;
 
         isAnyUIOpen = true;
     }
+
     public void CloseUI(int num)
     {
         PoPUpScreen[num].SetActive(false);
 
         isAnyUIOpen = false;
+    }
+
+    public Dictionary<int, Dictionary<string, string>> ReadCSV(TextAsset file)
+    {
+        string[] key = null;
+        string[] value = null;
+        Dictionary<string, string> dataset = new Dictionary<string, string>();
+        Dictionary<int, Dictionary<string, string>> eventdata = new Dictionary<int, Dictionary<string, string>>();
+        int num = 0;
+
+        string[] data = file.text.Split('\n');
+        foreach (string s in data)
+        {
+            if (string.IsNullOrEmpty(s)) break;
+            if (s.Substring(0, 1).Equals("!")) break;
+
+            dataset = new Dictionary<string, string>();
+
+            if (s.Substring(0, 1).Equals("#")) // 첫 줄에 각 항목에 대한 간단한 설명이 들어가고, 이를 읽어 딕셔너리 키로 사용
+            {
+                key = s.Split(',');
+                key[0] = key[0].Replace("#", "");
+                key[6] = key[6].Replace("\r\n", "");
+                continue;
+            }
+
+            value = s.Split(',');
+            value[6] = value[6].Replace("\r\n", "");
+            for (int i = 0; i < key.Length; i++)
+                dataset.Add(key[i], value[i]);
+
+            eventdata.Add(num, dataset);
+            //dataset.Clear();
+            num += 1;
+        }
+
+        return eventdata;
     }
 }
--- Assets/Script/UI/ShopUI.cs
+++ Assets/Script/UI/ShopUI.cs
@@ -1,24 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Random = System.Random;
 
 public class ShopUI : MonoBehaviour
 {
+    public enum Product
+    {
+        BATTLEAXE = 0,
+        DAGGER = 1,
+        HAMMER = 2,
+        SHIELD = 3,
+        SWORD = 4,
+        BLIZZARD = 5,
+        CHAINLIGHTNING = 6,
+        FIREBALL = 7,
+        FLAMESTEP = 8,
+        FROSTNOVA = 9,
+        THUNDERBOLT = 10
+    }
+    public List<Product> products = new List<Product>();
+    public Random random = new Random();
+
+    public Canvas[] shelf;
+    public Sprite[] images = new Sprite[11];
+
+
     void Start()
     {
+        for (int i = 0; i <= 11; i++)
+            products.Add((Product)i);
+        shelf = GetComponentsInChildren<Canvas>();
+
+
         this.gameObject.SetActive(false);
     }
 
+    void Update()
+    {
+        //골드 창에 현재 골드 계속 갱신 필요
+    }
+
+    public void OpenShop()
+    {
+        for (int i = 0; i <= 5; i++)
+            products.RemoveAt(random.Next(0, products.Count));
+
+        for(int i = 0; i < shelf.Length - 1; i++)
+        {
+            Image[] thumbnails = shelf[i + 1].GetComponentsInChildren<Image>();
+            Button[] buttons = shelf[i + 1].GetComponentsInChildren<Button>();
+            int count = 0;
+            for(int j = 0; j < 3; j++)
+            {
+                thumbnails[count].sprite = images[(int)products[j + (i * 3)]];
+                // 플레이어 연동해서 레벨 비례 가격 책정
+                //buttons[count].GetComponentInChildren<Text>().text = 
+                count++;
+            }
+        }
+            
+    }
+
+    public void Purchase()
+    {
+        // 플레이어 연동해서 상품 가격과 비교, 돈이 없으면 그냥 무시 = return
+        // 돈이 있으면 썸네일 이미지 색 어둡게, 버튼 못누르게 변경
+        // 플레이어 무기 레벨도 +1 해줘야 함
+    }
+
+
     public void Close()
     {
+        for (int i = 0; i <= 11; i++)
+            products.Add((Product)i);
+
         MapUIManager.manager.CloseUI(1);
     }
 
-
-
-    public bool payable()
-    {
-
-        return false;
-    }
 }
